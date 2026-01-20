c4d82e6986d5e05b693098a16f661ad4b89ce266
siddhant385 <ssiddhant.ssharma@gmail.com>
2026-01-21 03:35:43 +0530

    Added basic Auth System with Supabase Auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Project Management System",
+  description: "Jabalpur Engineering College Project Management System built for academics and non academics project management",
 };
 
 const geistSans = Geist({
